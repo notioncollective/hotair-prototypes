83392fda60dc5abd4e5bebe01a543dfbbdd26547
Andy Dayton <amdayton@gmail.com>
2016-05-25 16:25:14 -0400

    Added notion logo for dragging

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var Crafty = require('craftyjs');
 
-Crafty.init();
-Crafty.background('rgb(150,200,255)');
+var assets = {
+	"images" : "assets/notion_logo.png"
+};
 
-Crafty.e('2D, WebGL, Color, Draggable')
-	.color(255, 255, 255, 1)
-  .attr({x: 13, y: 37, w: 50, h: 50})
+Crafty.load(assets, function() {
+
+	Crafty.init();
+	Crafty.background('rgb(150,200,255)');
+	Crafty.webgl.init();
+
+	var ctx = Crafty.webgl.context
+	var dims = { width: ctx.drawingBufferWidth, height: ctx.drawingBufferHeight };
+	var size = dims.width/5;
+
+
+	var logo = Crafty.e('2D, WebGL, Image, Draggable').image('assets/notion_logo.png')
+	  // .attr({x: dims.width/2-size/2, y: dims.height/2-size/2, w: size, h: size})
+
+
+	var info = Crafty.e('2D, Canvas, Text')
+							.attr({x: 5, y: dims.height - 15})
+							.text("Drag & drop the logo!")
+							.textColor('black');
+
+
+
+});
