9242d540ce9d0c345fc61b65f499e0e82bc8a88c
Andy Dayton <amdayton@gmail.com>
2016-05-25 16:02:03 -0400

    Added basic draggable element

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var Crafty = require('craftyjs');
 
-console.log('crafty', Crafty);
+Crafty.init();
+Crafty.background('rgb(150,200,255)');
+
+Crafty.e('2D, WebGL, Color, Draggable')
+	.color(255, 255, 255, 1)
+  .attr({x: 13, y: 37, w: 50, h: 50})
