191fb5437b930a63d327df917d8b261acd2e9573
Andy Dayton <amdayton@gmail.com>
2016-05-26 09:20:19 -0400

    WIP. Breaking into scenes

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ var assets = {
 	"images" : "assets/notion_logo.png"
 };
 
-function init() {
 
-	// setup crafty
-	Crafty.init();
-	Crafty.background('rgb(150,200,255)');
+// var gui = new dat.GUI();
+
+
+// loading scene
+Crafty.defineScene("loading", function() {
+	Crafty.background("#000");
+  Crafty.e("2D, Canvas, Text")
+	  .attr({ w: 100, h: 20, x: 150, y: 120 })
+	  .text("Loading")
+	  .textColor("#FFFFFF");
+});
+
+Crafty.defineScene("gravity",function() {
+	Crafty.background("rgb(150,200,255)");
+
 	Crafty.webgl.init();
-
-	// setup gui
-	var gui = new dat.GUI();
 
 	// other basic stuff
 	var ctx = Crafty.webgl.context
@@ -28,9 +36,15 @@ function init() {
 	// bottom platform
 	var platform = Crafty.e('2D, WebGL, Color')
 									.color(0, 255, 100, 1)
-									.attr({x: 0, y: dims.height-5, w: dims.width, h: 10});
+									.attr({x: 0, y: dims.height-5, w: dims.width, h: 10});	
+});
 
-}
+
+// setup crafty
+Crafty.init();
+Crafty.enterScene("loading");
 
 // load & go!
-Crafty.load(assets, init);
+Crafty.load(assets, function() {
+	Crafty.enterScene("gravity");
+});
